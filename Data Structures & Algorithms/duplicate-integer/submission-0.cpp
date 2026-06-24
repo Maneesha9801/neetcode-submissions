5b57114af3135e973e01d95f914c340bb895c551
Maneesha a pillai <77294239+Maneesha9801@users.noreply.github.com>
2026-06-24 23:43:59 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_set<int> check ;
+        for (int i = 0; i < nums.size() ; i++){
+            if (!check.count(nums[i])){
+                check.insert(nums[i]);
+            }
+            else return true;
+        }
+        return false;
+    }
+};
